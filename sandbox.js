dba22b1a850eb27bcfd070e2de3af6296e27eb37
borkarsaish65 <borkarsaish7@gmail.com>
2020-03-09 21:14:26 +0530

    V1

---
+++ sandbox.js
@@ -0,0 +1,23 @@
+ //cehck off specific todos
+ $("ul").on("click","li",function(){
+  $(this).toggleClass("completed");   
+ });
+
+ $("ul").on("click","span",function(e){
+  
+     $(this).parent().fadeOut(500,function(){
+         $(this).remove();
+     });
+     e.stopPropagation() 
+ })
+
+ $("input[type='text']").keypress(function(e){
+     if(e.which === 13 )
+     {
+         var text = $(this).val();
+         $("ul").append("<li><span><i class='fas fa-trash'></i> </span> " + text + "</li>")
+     };
+ })
+ $(".fa-pencil-alt").click(function(){
+     $("input[type='text']").fadeToggle();
+ })
